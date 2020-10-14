d34c3a7f6cd9b5ef4ae3fd03338cf2b71434647b
Jamil Matheny <jamil.matheny@gmail.com>
2020-10-14 10:02:52 -0400

    Add project files.

---
+++ GradeBook/GradeBook.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/* 
+ * Name: Jamil Matheny
+   Date: 10/14/2020
+   Class: CIS214 */
+
+namespace GradeBook
+{
+    public class GradeBook
+    {
+        // auto-implemented property CourseName implicity created an 
+        // instance variable for this GradeBook's course name
+        public string CourseName { get; set; }
+
+        // constructor initializes auto-implemented property
+        // CourseName with string supplied as argument
+        public GradeBook (string name)
+        {
+            CourseName = name; // set CourseName to name
+        } // end Constructor
+
+        public void DisplayMessage()
+        {
+            // use auto-implemented property CourseName to get the name of the course 
+            // that this GradeBook represents
+            Console.WriteLine("Welcome to the grade book for\n{0}!", CourseName);
+
+        } // end method DisplayMessage
+    } // end class GradeBook
+} // end namespace
---
+++ GradeBook/GradeBookTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+/* 
+ * Name: Jamil Matheny
+   Date: 10/14/2020
+   Class: CIS214 */
+namespace GradeBook
+{
+    public class GradeBookTest
+    {
+        public static void Main(string[] args)
+        {
+            // Create a GradeBook object
+            GradeBook gradeBook1 = new GradeBook( // invokes constructor
+                "CS101 Introduction to C# Programming" );
+            GradeBook gradeBook2 = new GradeBook( // invokes constructor
+                "CS102 Data Structures in C#");
+
+            // display initial value of CourseName
+            Console.WriteLine("gradeBook1 course name is: {0}", gradeBook1.CourseName);
+            Console.WriteLine("gradeBook2 course name is: {0}", gradeBook2.CourseName);
+
+        } // end Main
+    } // end class GradeBookTest
+}
